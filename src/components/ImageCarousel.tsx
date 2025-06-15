2dede37f1091abe21bf3a9b53b3be97cf788284d
CadoreBrx <132395314+CadoreBrx@users.noreply.github.com>
2025-06-15 11:54:48 -0300

    Update ImageCarousel.tsx

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -1,24 +1,23 @@
-// src/components/ImageCarousel.tsx
 import React, { useState, useEffect } from 'react';
 import './ImageCarousel.css';
 
 import casaImg from '../../src/img/casa.jpg';
-//import cattaniServicosLGImg from '../../src/img/cattaniServicosLG.jpg';
+// import cattaniServicosLGImg from '../../src/img/cattaniServicosLG.jpg';
 import obraParqueImg from '../../src/img/obraParque.jpg';
-//import paredeImg from '../../src/img/parede.jpg';
-//import pinturaEstacionamentoImg from '../../src/img/pinturaEstacionamento.jpg';
+// import paredeImg from '../../src/img/parede.jpg';
+// import pinturaEstacionamentoImg from '../../src/img/pinturaEstacionamento.jpg';
 import pinturaEstacionamento2Img from '../../src/img/pinturaEstacionamento2.jpg';
 import vidroImg from '../../src/img/vidro.jpg';
-import casa2 from '../img/casa2.jpg'
-import massa from '../img/massa.jpg'
- 
+import casa2 from '../img/casa2.jpg';
+import massa from '../img/massa.jpg';
+
 const images: string[] = [
   casaImg,
   casa2,
-  //cattaniServicosLGImg,
+  // cattaniServicosLGImg,
   obraParqueImg,
-  //paredeImg,
-  //pinturaEstacionamentoImg,
+  // paredeImg,
+  // pinturaEstacionamentoImg,
   pinturaEstacionamento2Img,
   massa,
   vidroImg,
@@ -35,29 +34,23 @@ const ImageCarousel: React.FC = () => {
     return () => clearInterval(intervalId);
   }, []);
 
-  const goToPrevious = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-  };
-
-  const goToNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
   return (
-  <div className="image-carousel">
-    {/* REMOVA ESTA LINHA */}
-    {/* <button className="carousel-button prev" onClick={goToPrevious}>
-      &lt;
-    </button> */}
-    <div className="carousel-image-container">
-      <img src={images[currentIndex]} alt={`Imagem ${currentIndex + 1}`} className="carousel-image" />
+    <div className="image-carousel">
+      {/* <button className="carousel-button prev" onClick={goToPrevious}>
+        &lt;
+      </button> */}
+      <div className="carousel-image-container">
+        <img
+          src={images[currentIndex]}
+          alt={`Imagem ${currentIndex + 1}`}
+          className="carousel-image"
+        />
+      </div>
+      {/* <button className="carousel-button next" onClick={goToNext}>
+        &gt;
+      </button> */}
     </div>
-    {/* REMOVA ESTA LINHA */}
-    {/* <button className="carousel-button next" onClick={goToNext}>
-      &gt;
-    </button> */}
-  </div>
-);
+  );
 };
 
 export default ImageCarousel;
